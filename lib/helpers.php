9099d71b507e963cd4695afbaccefc649983dc0a
Dampe <isdampe@gmail.com>
2015-01-09 10:39:13 +1100

    Fixed broken 301 plugin

--- lib/helpers.php
+++ lib/helpers.php
@@ -20,63 +20,6 @@ function custom_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
-function nazoma_write_301_route( $data ) {
-
-	//Write data to .htaccess.
-	$htaccess = get_home_path() . ".htaccess";
-
-	if ( file_exists( $htaccess) && filesize($htaccess) > 0 ) {
-
-		$fh = fopen($htaccess, "r");
-		$fhdata = fread($fh, filesize($htaccess) );
-		fclose($fh);
-
-		$start = strpos( $fhdata, "BEGIN WordPress" ) -2;
-
-		$ourstring = "#STARTNAZOMA#\n";
-		$ourstring = "RewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\n";
-		foreach ( $data as $line ) {
-
-			$ourstring .= "RewriteRule ^". str_replace("\n", "", $line['original']) . "$ " . $line['301'] . "$1 [L,NC,R=301]\n";
-		}
-
-
-		//Is there a previous entry?
-		if ( $start > 0 ) {
-			//Yes.
-			//Append the data.
-			$newdata = $ourstring . substr( $fhdata, $start );
-		} else {
-			$newdata = $ourstring;
-		}
-
-
-
-	} else {
-		$newdata = null;
-		$ourstring = "#STARTNAZOMA#\n";
-		$ourstring = "RewriteEngine on\nRewriteCond %{REQUEST_FILENAME} !-f\n";
-		foreach ( $data as $line ) {
-
-			$ourstring .= "RewriteRule ^".$line['original'] . "$ " . $line['301'] . "$1 [L,NC,R=301]\n";
-		}
-
-
-		$newdata = $ourstring;
-
-	}
-
-	//Write the file.
-	$fh = fopen($htaccess, "w");
-	if (! $fh ) {
-		add_action('admin_notices', 'nazoma_admin_notice');
-	}
-	fwrite( $fh, $newdata );
-
-	fclose($fh);
-
-
-}
 
 function nazoma_admin_notice() {
 	echo '<div class="error"><p>Sorry, the .htaccess file doesnt seem writable.</p></div>';
--- lib/hooks.php
+++ lib/hooks.php
@@ -25,48 +25,32 @@ add_action( 'admin_head', 'nazoma_hide_fields' );
 
 add_theme_support( 'post-thumbnails' );
 
-function nazoma_hook_301() {
+function nazoma_301() {
+	//die("ha");
+	$rules = esc_attr( get_option( 'nazoma_301' ) );
+	
+	if ( $rules ) {
 
-	global $pagenow;
+		$lines = explode("\n", $rules);
+		foreach( $lines as $line ) {
 
-	if ( ! current_user_can( 'manage_options' ) && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
-		wp_die( __( 'You are not allowed to access this part of the site' ) );
-		return false;
-		exit;
-	}
+			$data = explode("::::", $line);
+			if ( count($data) == 2 ) {
 
-	$screen = get_current_screen();
-	if ( $screen->id == "toplevel_page_nazoma-integration/lib/settings" ) {
+				//Check request for 301.
+				$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-		//Only execute on settings page.
-		$val = esc_attr( get_option('nazoma_301') );
-		if ( ! $val ) {
-			return false;
-		}
-
-		$array = explode( "\r", $val );
-		$too = array();
-
-		foreach( $array as $line ) {
-			if ( $line ) {
-				$ex = explode("::::", $line);
-
-				if ( count($ex) >= 2 ) {
-					$i = count($too);
-
-					$too[$i]['original'] = str_replace(".", "\.", $ex[0] );
-					$too[$i]['301'] = str_replace("\n", "", $ex[1]);
+				if ( $data[0] == $actual_link ) {
+					header("HTTP/1.1 301 Moved Permanently");
+					header("Location: " . $data[1] ); 
+					exit;
 				}
+
 			}
+
 		}
 
-		nazoma_write_301_route( $too );
-
-	} else {
-		return false;
 	}
-
-
 
 }
-add_action( 'admin_enqueue_scripts', 'nazoma_hook_301' );
+add_action( 'init', 'nazoma_301' );
